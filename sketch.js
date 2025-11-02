ccf06609476cbdbcee245a40d1d44c4cb839ea5c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-02 17:52:13 +0100

    feat: funy img

--- sketch.js
+++ sketch.js
@@ -1,36 +1,34 @@
-// sketch.js
+console.log("Hello, textmode.js!");
 const t = textmode.create({
   width: window.innerWidth,
   height: window.innerHeight,
   fontSize: 16,
   frameRate: 60,
 });
+const tm = t;
 
-const hello = ": HELLO, WORLD! :";
-
-t.setup(() => {
-  // Optional setup code here (e.g., load fonts/shaders, initialize variables that access 't')
+t.setup(async () => {
+  myImage = await t.loadImage("juan.png");
 });
 
-t.draw(() => {
+t.draw(async () => {
   t.background(0);
-
-  const time = t.frameCount * 0.3;
 
   for (let y = 0; y < t.grid.rows; y++) {
     for (let x = 0; x < t.grid.cols; x++) {
-      t.push();
+      tm.push();
 
-      pos = Math.round(time) + x + 4 * y;
+      tm.char(".");
+      tm.charColor(120, 0, 255);
 
-      t.char(hello[pos % hello.length]);
-      t.charColor((24 * 255) / (x + y + 1), 0, 0);
-      t.cellColor(0, 0, 0);
-      t.rect(x, y, 1, 1);
+      tm.cellColor(0, 0, 0);
+      tm.rect(x, y, 1, 1);
 
-      t.pop();
+      tm.pop();
     }
   }
+
+  t.image(myImage, 0, 0, t.grid.cols, t.grid.rows);
 });
 
 t.windowResized(() => {
