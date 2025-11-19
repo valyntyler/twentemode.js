0147648d5b539ac47882406e2e9b3194bb547f39
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-19 18:44:57 +0100

    chore: remove shader

--- sketch.js
+++ sketch.js
@@ -11,44 +11,6 @@ t.setup(async () => {
   myImage = await t.loadImage("twente.svg");
 });
 
-const noiseShader = t.createFilterShader(`#version 300 es
-precision highp float;
-
-in vec2 v_uv;
-in vec3 v_character;
-in vec4 v_primaryColor;
-in vec4 v_secondaryColor;
-in vec2 v_rotation;
-in vec3 v_transform;
-
-uniform float u_frameCount;
-
-layout(location = 0) out vec4 o_character;
-layout(location = 1) out vec4 o_primaryColor;
-layout(location = 2) out vec4 o_secondaryColor;
-layout(location = 3) out vec4 o_rotation;
-layout(location = 4) out vec4 o_transform;
-
-float random(vec2 st) {
-  return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
-}
-
-void main() {
-  vec2 gridPos = floor(gl_FragCoord.xy);
-  float noise = random(gridPos + u_frameCount * 0.1);
-
-  o_character = vec4(0.01, 0.0, 0.0, 1.0);
-  o_primaryColor = vec4(vec3(1.0), 1.0);
-  o_secondaryColor = vec4(0.0, 0.0, 0.0, 1.0);
-  o_rotation = vec4(0.0, 0.0, 0.0, 1.0);
-  o_transform = vec4(0.0, 0.0, 0.0, 1.0);
-}`);
-
-// t.draw(() => {
-//   t.shader(noiseShader);
-//   t.setUniform('u_frameCount', t.frameCount);
-//   t.rect(0, 0, t.grid.cols, t.grid.rows);
-// });
 
 t.draw(async () => {
   t.background(0);
@@ -68,7 +30,6 @@ t.draw(async () => {
     }
   }
 
-  t.shader(noiseShader);
   // t.image(myImage, 0, 0, t.grid.cols, t.grid.rows);
 });
 
