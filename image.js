ad8172931c53bfb0cf9bb4ce7334556826b3ffbe
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-19 19:50:49 +0100

    refactor: create image.js

---
+++ image.js
@@ -0,0 +1,24 @@
+export function loadImage(path, resolution) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.crossOrigin = "anonymous";
+    img.src = path;
+
+    img.onerror = () => {
+      reject(new Error(`Failed to load image: ${path}`));
+    };
+
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = resolution[0];
+      canvas.height = resolution[1];
+
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+      const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      resolve(data);
+    };
+  })
+}
--- sketch.js
+++ sketch.js
@@ -1,27 +1,4 @@
-function loadImage(path, resolution) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-
-    img.crossOrigin = "anonymous";
-    img.src = path;
-
-    img.onerror = () => {
-      reject(new Error(`Failed to load image: ${path}`));
-    };
-
-    img.onload = () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = resolution[0];
-      canvas.height = resolution[1];
-
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-      const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      resolve(data);
-    };
-  })
-}
+import { loadImage } from "./image.js";
 
 (async () => {
   const jeff = await loadImage("juan.png", [100, 100])
