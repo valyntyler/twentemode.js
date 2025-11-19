5cd21c9eab2899457b0b76f90c983e1ebb41f020
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-19 22:45:49 +0100

    fmt: image.js

--- image.js
+++ image.js
@@ -10,31 +10,31 @@ export function loadImage(path, resolution) {
     };
 
     img.onload = () => {
-      const canvas = document.createElement('canvas');
+      const canvas = document.createElement("canvas");
       canvas.width = resolution[0];
       canvas.height = resolution[1];
 
-      const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext("2d");
       ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
       const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
       resolve(data);
     };
-  })
+  });
 }
 
 export function toImageSpace(position, resolution) {
-  const width = resolution[0]
-  const x = position[0]
-  const y = position[1]
-  return ((y * width) + x) * 4
+  const width = resolution[0];
+  const x = position[0];
+  const y = position[1];
+  return (y * width + x) * 4;
 }
 
 export function imageRGB(image, position, resolution) {
-  const pixelIndex = toImageSpace(position, resolution)
+  const pixelIndex = toImageSpace(position, resolution);
   return [
     image.data[pixelIndex],
     image.data[pixelIndex + 1],
-    image.data[pixelIndex + 2]
+    image.data[pixelIndex + 2],
   ];
 }
